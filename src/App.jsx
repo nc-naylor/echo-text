c0a9e5a6d5086a94e71fa900572cff365f284f21
nc-naylor <natasha.c.naylor@gmail.com>
2024-08-20 12:00:15 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1 className='text-black-400'>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
